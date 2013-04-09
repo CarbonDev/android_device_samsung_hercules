b46ccd2e7585beb60e9d65063553f2123323ebde
root <root@android-RS880M06-8KRS3HX.(none)>
2013-04-09 13:53:14 -0700

    Carbonify

--- full_hercules.mk
+++ full_hercules.mk
@@ -33,7 +33,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/hercules/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_hercules
+PRODUCT_NAME := carbon_hercules
 PRODUCT_DEVICE := hercules
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
